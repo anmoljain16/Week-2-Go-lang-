df54fb838cde7d1b889f7e2a839dd7b5e40f282a
anmoljain16 <ajanmoldosi@gmail.com>
2023-02-04 21:07:26 +0530

    Commiting changes

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/shivpalyadavv/week2-GL1-CipherSchools/database"
+	"github.com/shivpalyadavv/week2-GL1-CipherSchools/routers"
+	"log"
+)
+
+func main() {
+	database.Setup()                    //establish the dtatbase connection
+	engine := routers.Router()          //get the customized engine
+	err := engine.Run("127.0.0.1:8080") // start the engine
+	if err != nil {
+		log.Fatal(err)
+	}
+}
